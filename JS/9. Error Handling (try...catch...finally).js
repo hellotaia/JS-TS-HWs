cb7f6155df2aeffc84046a88de8ad0c630054cc5
TaisiiaT <taisiia.teslenko@redsailtechnologies.com>
2026-01-27 00:56:20 +0100

    update 9 task

--- JS/9. Error Handling (try...catch...finally).js
+++ JS/9. Error Handling (try...catch...finally).js
@@ -2,18 +2,29 @@
 //Robust User Data Processor
 
 function processUserData(users) {
-    for (const user of users) {
-        if (typeof user.name !== 'string' || user.name.trim() === '') {
-            console.log(`Processing user: [${user.name}]`)
-            throw new Error(`Invalid user name. (Name: [${user.name}])`);
+    try{
+    if (!Array.isArray(users)) {
+        throw new Error(`Invalid input: expected data is not an array, got ${typeof users}`);
+    }
 
+    for (const user of users) {
+            console.log(`Processing user: [${user.name}]`);
+        if (typeof user.name !== 'string' || user.name.trim() === '') {
+            throw new Error(`Invalid user name. (Name: [${user.name}])`);
         }
         if (typeof user.age !== 'number' || user.age < 0) {
-            console.log(`Processing user: [${user.name}]`)
             throw new Error(`Invalid user age. (Age: [${user.age}])`)
         }
-        console.log(`[GREAT SUCCSESS] Processing user: [${user.name}]`)
+        console.log(`[GREAT SUCCSESS] Processing user: [${user.name}]`);
     }
+    return `All users processed successfully. Total users: ${users.length}`;
+}
+catch (e) {
+    console.error('[ERROR] processing data:', e.message)
+}
+finally {
+    console.log('Valid Users processing cycle complete')
+}
 }
 
 const validUsers = [
@@ -26,27 +37,17 @@ const invalidUsers = [
     { name: 21, age: 20 },
     { name: 'Vova', age: -2 },
     { name: ' ', age: 'one' },
-    { name: null, age: 20 }
+    { name: null, age: 20 },
+    {}
 ];
 
-try {
-    console.log(`--- Valid users check: `);
-    processUserData(validUsers);
-}
-catch (e) {
-    console.error('[ERROR] processing data:', e.message)
-}
-finally {
-    console.log('Valid Users processing cycle complete')
-}
 
-try {
+    console.log(`--- Valid users check: `);
+    console.log(processUserData(validUsers));
+
     console.log("--- Invalid users check: ");
-    processUserData(invalidUsers);
-}
-catch (e) {
-    console.error('[ERROR] processing data: ', e.message)
-}
-finally {
-    console.log('Invalid Users processing cycle complete')
-}
+    console.log(processUserData(invalidUsers));
+
+    console.log('--- Not an array check: ');
+    console.log(processUserData('Not an array'));
+    console.log('--- End of user data processing ---');
