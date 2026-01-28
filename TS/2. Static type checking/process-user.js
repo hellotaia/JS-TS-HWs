c625b1ca42572a309914d0038356f2871ed1fa9b
TaisiiaT <taisiia.teslenko@redsailtechnologies.com>
2026-01-28 16:11:35 +0100

    TS 2 - HW

---
+++ TS/2. Static type checking/process-user.js
@@ -0,0 +1,26 @@
+var user = {
+    name: "Daniel",
+    age: 26,
+    getGreeting: function () {
+        return "Hello, my name is ".concat(this.name, ".");
+    }
+};
+//bug 2 Fix: add email type definition
+//function processUser(userObject) {
+function processUser(userObject) {
+    // Bug 1: Typo in a standard method name.
+    var announcement = "Processing user data...";
+    // console.log(announcement.toLocalLowerCase());
+    // Fix: typo from toLocalLowerCase to toLocaleLowerCase
+    console.log(announcement.toLocaleLowerCase());
+    // Bug 2: Trying to access a property that doesn't exist
+    // Fix: add type definition and optional check for email?: to function parameter
+    console.log("User's email: ".concat(userObject.email));
+    // Bug 3: Forgetting to call a function/method
+    // const greeting = userObject.getGreeting;
+    // if (greeting) {
+    // console.log("Greeting function exists.");}
+    // Fix: Call the function, removed unnecessary check
+    console.log(userObject.getGreeting());
+}
+processUser(user);
